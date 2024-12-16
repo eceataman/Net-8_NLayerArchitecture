05a69e530bcf3f9a04f4c38978203bb7445a68f4
Ece Ataman <114909014+eceataman@users.noreply.github.com>
2024-12-16 23:39:53 +0300

    Add project files.

---
+++ Repositories/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Repositories/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+   public class Product
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; } = default!;
+        // int değere default olarak 0 atanır ama string oldugunda null değeri olmasın diye bu yazılır
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
